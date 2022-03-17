97457ccb5d4798f7ff6999ac2c4848827a5373a8
Waqar <waqarkhan7400@gmail.com>
2022-03-17 20:40:38 +0530

    init

--- src/App.js
+++ src/App.js
@@ -1,30 +1,128 @@
 import React from 'react';
 
-
 export class App extends React.Component{
 
   constructor(props){
     super(props);
     this.state={
-      buyItems:['milk','bread','cake']
-    }
+      buyItems:['milk','bread','cake','coffee'],
+      message:''
 
+    }
   }
 
+  addItem(e){
+    e.preventDefault();
+    const{buyItems} = this.state;
+    const newItem = this.newItem.value;
+
+    const isOnTheList= buyItems.includes(newItem);
+
+    if(isOnTheList){
+        this.setState({
+          message: 'This item is already on the list.' 
+        })
+    }
+    else{
+
+      newItem !=='' && this.setState({
+        buyItems: [...this.state.buyItems,newItem],
+        message: ''
+      })
+
+    }
+
+    this.addForm.reset();
+  }
+
+  removeItem(items){
+    const newBuyItems= this.state.buyItems.filter(buyItems=>{
+      return buyItems !== items;
+    })
+
+    this.setState({
+      buyItems: [...newBuyItems]
+    })
+
+     if(newBuyItems.length === 0){
+       this.setState({
+         message: 'No items on the list yet! Add some :)'
+       })
+     }
+  }
+
+  clearAll(){
+    this.setState({
+      buyItems: [],
+      message: 'No items on the list yet! Add some :)'
+    })
+  }
+
+  
+
   render(){
-    const {buyItems}= this.state;
-    return (
+    const {buyItems, message}=this.state;
+
+
+    return(
       <div>
-        <h1>Shopping List</h1> 
-        {
-          this.state.buyItems.map(items=>{
-            return <p key={items}>  {items}</p>
-          })
-        }  
+        <header>
+          
+          <h3 className="head">Shopping-List</h3>
+
+          <form ref={input => this.addForm = input} className="form-inline" onSubmit={(e) => {this.addItem(e)}}>
+            <div className="form-group">
+                <input ref={input => this.newItem = input} type="text" placeholder="add your item" className="form-control" id="newItemInput"/>
+                <button type="submit" className="btn btn-primary">Add</button>
+            </div>
+            
+          </form>
+          
+        </header>
+
+        <div className="content">
+           {
+             (message !== '' || buyItems.length === 0) && <p className="message text-danger">{message}</p>
+           }
+
+           {
+             buyItems.length > 0 &&
+
+             <table className="table table-dark">
+             {/* <caption>Shopping List</caption> */}
+            <thead>
+               <tr>
+                  
+                  <th>Item</th>
+                  <th>Action</th>     
+               </tr>
+            </thead>
+           <tbody>
+
+              {
+                buyItems.map(items =>{
+                return (
+                 <tr key={items}>
+                   
+                   <td>{items}</td>
+                   <td className="text-right">
+                     <button onClick={(e)=>this.removeItem(items)} type="button" className="btn btn-secondary btn-sm">Remove</button>
+                   </td>
+                 </tr>
+                )
+                 })
+              }
+
+              
+          </tbody>
+       </table>
+
+           }
+          
+          <button onClick={(e) => this.clearAll()} className="btn btn-secondary btn-sm">Clear List</button>
+        </div>
+        
       </div>
     )
   }
 }
-
-
-
