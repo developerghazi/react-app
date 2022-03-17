54e2eee7a217f568ede40c75ffa0af067d026244
Waqar <waqarkhan7400@gmail.com>
2022-03-17 20:00:12 +0530

    init

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+export class App extends React.Component{
+
+  constructor(props){
+    super(props);
+    this.state={
+      buyItems:['milk','bread','cake']
+    }
+
+  }
+
+  render(){
+    const {buyItems}= this.state;
+    return (
+      <div>
+        <h1>Shopping List</h1> 
+        {
+          this.state.buyItems.map(items=>{
+            return <p key={items}>  {items}</p>
+          })
+        }  
+      </div>
+    )
+  }
 }
 
-export default App;
+
+
